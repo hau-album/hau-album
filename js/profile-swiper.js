a1dcf5a2016d0e4693ac74fe099c434787c58111
hau-album <158164352+hau-album@users.noreply.github.com>
2024-01-31 23:26:04 +0900

    Add files via upload

---
+++ js/profile-swiper.js
@@ -0,0 +1,42 @@
+const costume = new Swiper('#costume-swiper', {
+  loop:true,
+  autoplay:{
+    delay:5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+const illust = new Swiper('#illust-swiper', {
+  slidesPerView: 1,
+  spaceBetween:100,
+  loop:true,
+  autoplay:{
+    delay:5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+const sdillust = new Swiper('#sdillust-swiper', {
+  slidesPerView: 1,
+  breakpoints: {
+    1024:{
+      slidesPerView: 3,
+    }
+  },
+  spaceBetween:30,
+  loop:true,
+  autoplay:{
+    delay:5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
